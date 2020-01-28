937df85650d011ae5ef6bd92f1d33c7c001491cd
Seyedpouya hajiseyedjavadi <pouya.javadi.resume@gmail.com>
2020-01-28 05:48:14 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -13,7 +13,7 @@ class App < Sinatra::Base
   
   post '/checkout' do
     
-    session["item"] = Checkout.new(params[:item])
-    @session = session
+    @session["item"] = Checkout.new(params[:item])
+    
   end
 end
